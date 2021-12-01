da76d6456e6e78c6f0b62b787f1d32fd45521ad8
Andriy Podranetskyy <apodran@gmail.com>
2021-12-01 17:58:58 +0200

    Better options parse

--- file_diff.pl
+++ file_diff.pl
@@ -20,10 +20,22 @@
 
 use strict;
 use warnings;
+use utf8;
 
+use Getopt::Long;
 use List::Compare;
 
-my ( $file1, $file2, $option ) = @ARGV;
+my ( $difference, $intersection, $usage );
+
+GetOptions(
+    "difference"   => \$difference,      # boolean
+    "intersection" => \$intersection,    # boolean
+    "help|usage"   => \$usage,
+) or usage( -die => 1 );
+
+usage() if $usage;
+
+my ( $file1, $file2 ) = @ARGV;
 
 unless ( $file1 && $file2 ) {
     die "Two existing files should be given!\n";
@@ -40,28 +52,28 @@ unless ( -r $file2 ) {
 my $INFILE1_file_name = $file1;    # input file name
 
 open my $INFILE1, '<', $INFILE1_file_name
-    or die "$0 : failed to open  input file $INFILE1_file_name : $!\n";
+  or die "$0 : failed to open  input file $INFILE1_file_name : $!\n";
 
 my @lines1 = <$INFILE1>;
 
 close $INFILE1
-    or warn "$0 : failed to close input file $INFILE1_file_name : $!\n";
+  or warn "$0 : failed to close input file $INFILE1_file_name : $!\n";
 
 my $INFILE2_file_name = $file2;    # input file name
 
 open my $INFILE2, '<', $INFILE2_file_name
-    or die "$0 : failed to open  input file $INFILE2_file_name : $!\n";
+  or die "$0 : failed to open  input file $INFILE2_file_name : $!\n";
 
 my @lines2 = <$INFILE2>;
 
 close $INFILE2
-    or warn "$0 : failed to close input file $INFILE2_file_name : $!\n";
+  or warn "$0 : failed to close input file $INFILE2_file_name : $!\n";
 
 # Compare lines: only lines of file1 not presented in file2 will be shown
 my $lc = List::Compare->new( \@lines1, \@lines2 );
 
 my @list;
-if ( $option && $option eq '-i' ) {
+if ($intersection) {
     @list = $lc->get_intersection;
 }
 else {
@@ -69,3 +81,35 @@ else {
 }
 
 print foreach @list;
+
+exit;
+
+sub usage {
+    my %args = (
+        -die => 0,
+        @_
+    );
+
+    print << "USAGE";
+Synopsis: $0 <file1> <file2> OPTIONS
+
+Command-specific options:
+   --difference
+      Show a difference of two files (default option)
+   --intersection
+      Show an intersection of two files instead of a difference
+   --help
+   --usage
+      This message
+
+Examples:
+    $0 file1.txt file2.txt
+    $0 -i file1.txt file2.txt
+    $0 file1.txt file2.txt --intersection
+    $0 --difference file1.txt file2.txt
+USAGE
+
+    exit( $args{-die} );
+
+    return;
+}
